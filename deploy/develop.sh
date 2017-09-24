8c50afb0accf4ed0b27a01142e69afdc9eced77a
Tiross <Tiross@users.noreply.github.com>
2017-09-24 17:49:16 +0200

    Use HTTPS with auto-credentials

--- deploy/develop.sh
+++ deploy/develop.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+git remote set-url origin https://${GITHUB_CREDENTIALS}@github.com/${TRAVIS_REPO_SLUG}.git
+git fetch origin
+
 git add build
 git commit -m '[ci skip] Automatic build update'
 git push origin HEAD:develop
